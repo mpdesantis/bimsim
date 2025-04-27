9c11979f623ffd642cefe2008c78dd61adc49ee0
Michael De Santis <michaelpeterdesantis@gmail.com>
2025-04-27 15:24:33 -0400

    initialize example template

--- run_all_examples.sh
+++ run_all_examples.sh
@@ -35,68 +35,62 @@ echo -e "EXECUTING $SCRIPT...\n"
 BIN_DIR=${PWD}/bin
 SIMULATION_BINARY=${BIN_DIR}/bimsim
 
-# Config
-CONFIG_DIR=${PWD}/config
+# Duration
+DEFAULT_SIM_TIME=96
 
-EX01_CONFIG_DIR=${PWD}/config/ex01
-EX01_V01_CONFIG=${EX01_CONFIG_DIR}/ex01_v01_bimsim_config.json
-EX01_V02_CONFIG=${EX01_CONFIG_DIR}/ex01_v02_bimsim_config.json
-EX01_V03_CONFIG=${EX01_CONFIG_DIR}/ex01_v03_bimsim_config.json
+# Examples
+EXAMPLES_DIR=${PWD}/examples
 
-EX02_CONFIG_DIR=${PWD}/config/ex02
-EX02_V01_CONFIG=${EX02_CONFIG_DIR}/ex02_v01_bimsim_config.json
-EX02_V02_CONFIG=${EX02_CONFIG_DIR}/ex02_v02_bimsim_config.json
-EX02_V03_CONFIG=${EX02_CONFIG_DIR}/ex02_v03_bimsim_config.json
+# Ex01 
+EX01_DIR=${EXAMPLES_DIR}/ex01
+EX01_V01_DIR=${EX01_DIR}/v01
+EX01_V01_CONFIG=${EX01_V01_DIR}/ex01_v01_bimsim_config.json
+EX01_V01_OUTPUT=${EX01_V01_DIR}/ex01_v01_bimsim_grid_log.csv
+EX01_V02_DIR=${EX01_DIR}/v02
+EX01_V02_CONFIG=${EX01_V02_DIR}/ex01_v02_bimsim_config.json
+EX01_V02_OUTPUT=${EX01_V02_DIR}/ex01_v02_bimsim_grid_log.csv
 
-EX03_CONFIG_DIR=${PWD}/config/ex03
-EX03_V01_CONFIG=${EX03_CONFIG_DIR}/ex03_v01_bimsim_config.json
-EX03_V02_CONFIG=${EX03_CONFIG_DIR}/ex03_v02_bimsim_config.json
-EX03_V03_CONFIG=${EX03_CONFIG_DIR}/ex03_v03_bimsim_config.json
+# Ex02
+EX02_DIR=${EXAMPLES_DIR}/ex02
+EX02_V01_DIR=${EX02_DIR}/v01
+EX02_V01_CONFIG=${EX02_V01_DIR}/ex02_v01_bimsim_config.json
+EX02_V01_OUTPUT=${EX02_V01_DIR}/ex02_v01_bimsim_grid_log.csv
+EX02_V02_DIR=${EX02_DIR}/v02
+EX02_V02_CONFIG=${EX02_V02_DIR}/ex02_v02_bimsim_config.json
+EX02_V02_OUTPUT=${EX02_V02_DIR}/ex02_v02_bimsim_grid_log.csv
 
-# Output
-OUTPUT_DIR=${PWD}/output
-
-EX01_V01_OUTPUT=${OUTPUT_DIR}/ex01_v01_bimsim_grid_log.csv
-EX01_V02_OUTPUT=${OUTPUT_DIR}/ex01_v02_bimsim_grid_log.csv
-EX01_V03_OUTPUT=${OUTPUT_DIR}/ex01_v03_bimsim_grid_log.csv
-
-EX02_V01_OUTPUT=${OUTPUT_DIR}/ex02_v01_bimsim_grid_log.csv
-EX02_V02_OUTPUT=${OUTPUT_DIR}/ex02_v02_bimsim_grid_log.csv
-EX02_V03_OUTPUT=${OUTPUT_DIR}/ex02_v03_bimsim_grid_log.csv
-
-EX03_V01_OUTPUT=${OUTPUT_DIR}/ex03_v01_bimsim_grid_log.csv
-EX03_V02_OUTPUT=${OUTPUT_DIR}/ex03_v02_bimsim_grid_log.csv
-EX03_V03_OUTPUT=${OUTPUT_DIR}/ex03_v03_bimsim_grid_log.csv
+# Ex03
+EX03_DIR=${EXAMPLES_DIR}/ex03
+EX03_V01_DIR=${EX03_DIR}/v01
+EX03_V01_CONFIG=${EX03_V01_DIR}/ex03_v01_bimsim_config.json
+EX03_V01_OUTPUT=${EX03_V01_DIR}/ex03_v01_bimsim_grid_log.csv
+EX03_V02_DIR=${EX03_DIR}/v02
+EX03_V02_CONFIG=${EX03_V02_DIR}/ex03_v02_bimsim_config.json
+EX03_V02_OUTPUT=${EX03_V02_DIR}/ex03_v02_bimsim_grid_log.csv
 
 # Prototype
-PROTOTYPE_DIR=${PWD}/prototype
-PROTOTYPE_CONFIG=${PROTOTYPE_DIR}/bimsim_config.json
-PROTOTYPE_OUTPUT=${PROTOTYPE_DIR}/bimsim_grid_log.csv
+# PROTOTYPE_DIR=${PWD}/prototype
+# PROTOTYPE_CONFIG=${PROTOTYPE_DIR}/bimsim_config.json
+# PROTOTYPE_OUTPUT=${PROTOTYPE_DIR}/bimsim_grid_log.csv
 
 
 ###############################################################################
 # Run all simulation examples
 echo -e "Executing all simulation examples...\n"
 
-echo -e "Executing simulation prototype...\n"
-${SIMULATION_BINARY} -c ${PROTOTYPE_CONFIG} -o ${PROTOTYPE_OUTPUT} -d 50
-
 # Ex01
 #echo -e "Executing simulation example 01...\n"
-#${SIMULATION_BINARY} -c ${EX01_V01_CONFIG} -o ${EX01_V01_OUTPUT}
-#${SIMULATION_BINARY} -c ${EX01_V02_CONFIG} -o ${EX01_V02_OUTPUT}
-#${SIMULATION_BINARY} -c ${EX01_V03_CONFIG} -o ${EX01_V03_OUTPUT}
+#${SIMULATION_BINARY} -c ${EX01_V01_CONFIG} -o ${EX01_V01_OUTPUT} -d ${DEFAULT_SIM_TIME} 
+#${SIMULATION_BINARY} -c ${EX01_V02_CONFIG} -o ${EX01_V02_OUTPUT} -d ${DEFAULT_SIM_TIME}
 
 # Ex02
 #echo -e "Executing simulation example 02...\n"
-#${SIMULATION_BINARY} -c ${EX02_V01_CONFIG} -o ${EX02_V01_OUTPUT}
-#${SIMULATION_BINARY} -c ${EX02_V02_CONFIG} -o ${EX02_V02_OUTPUT}
-#${SIMULATION_BINARY} -c ${EX02_V03_CONFIG} -o ${EX02_V03_OUTPUT}
+#${SIMULATION_BINARY} -c ${EX02_V01_CONFIG} -o ${EX02_V01_OUTPUT} -d ${DEFAULT_SIM_TIME}
+#${SIMULATION_BINARY} -c ${EX02_V02_CONFIG} -o ${EX02_V02_OUTPUT} -d ${DEFAULT_SIM_TIME}
 
 # Ex03
 #echo -e "Executing simulation example 03...\n"
-#${SIMULATION_BINARY} -c ${EX03_V01_CONFIG} -o ${EX03_V01_OUTPUT}
-#${SIMULATION_BINARY} -c ${EX03_V02_CONFIG} -o ${EX03_V02_OUTPUT}
-#${SIMULATION_BINARY} -c ${EX03_V03_CONFIG} -o ${EX03_V03_OUTPUT}
+#${SIMULATION_BINARY} -c ${EX03_V01_CONFIG} -o ${EX03_V01_OUTPUT} -d ${DEFAULT_SIM_TIME}
+#${SIMULATION_BINARY} -c ${EX03_V02_CONFIG} -o ${EX03_V02_OUTPUT} -d ${DEFAULT_SIM_TIME}
 
 echo -e "\n--- YOU EXECUTED ---"
