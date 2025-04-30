eff67ba8b09d0e1cf2165a61b7362ebdde869a7e
Michael De Santis <michaelpeterdesantis@gmail.com>
2025-04-30 01:31:24 -0400

    update example script

--- run_all_examples.sh
+++ run_all_examples.sh
@@ -41,51 +41,100 @@ DEFAULT_SIM_TIME=96
 # Examples
 EXAMPLES_DIR=${PWD}/examples
 
-# Ex01 
+# Example 01
 EX01_DIR=${EXAMPLES_DIR}/ex01
-EX01_V01_DIR=${EX01_DIR}/v01
-EX01_V01_CONFIG=${EX01_V01_DIR}/ex01_v01_bimsim_config.json
-EX01_V01_OUTPUT=${EX01_V01_DIR}/ex01_v01_bimsim_grid_log.csv
-EX01_V02_DIR=${EX01_DIR}/v02
-EX01_V02_CONFIG=${EX01_V02_DIR}/ex01_v02_bimsim_config.json
-EX01_V02_OUTPUT=${EX01_V02_DIR}/ex01_v02_bimsim_grid_log.csv
+# Configuration 01
+EX01_C01_DIR=${EX01_DIR}/c01
+# Variation 01
+EX01_C01_V01_DIR=${EX01_C01_DIR}/v01
+EX01_C01_V01_CONFIG=${EX01_C01_V01_DIR}/ex01_c01_v01_bimsim_config.json
+EX01_C01_V01_OUTPUT=${EX01_C01_V01_DIR}/ex01_c01_v01_bimsim_grid_log.csv
+# Variation 02
+EX01_C01_V02_DIR=${EX01_C01_DIR}/v02
+EX01_C01_V02_CONFIG=${EX01_C01_V02_DIR}/ex01_c01_v02_bimsim_config.json
+EX01_C01_V02_OUTPUT=${EX01_C01_V02_DIR}/ex01_c01_v02_bimsim_grid_log.csv
+# Configuration 02
+EX01_C02_DIR=${EX01_DIR}/c02
+# Variation 01
+EX01_C02_V01_DIR=${EX01_C02_DIR}/v01
+EX01_C02_V01_CONFIG=${EX01_C02_V01_DIR}/ex01_c02_v01_bimsim_config.json
+EX01_C02_V01_OUTPUT=${EX01_C02_V01_DIR}/ex01_c02_v01_bimsim_grid_log.csv
+# Variation 02
+EX01_C02_V02_DIR=${EX01_C02_DIR}/v02
+EX01_C02_V02_CONFIG=${EX01_C02_V02_DIR}/ex01_c02_v02_bimsim_config.json
+EX01_C02_V02_OUTPUT=${EX01_C02_V02_DIR}/ex01_c02_v02_bimsim_grid_log.csv
 
-# Ex02
+# Example 02
 EX02_DIR=${EXAMPLES_DIR}/ex02
-EX02_V01_DIR=${EX02_DIR}/v01
-EX02_V01_CONFIG=${EX02_V01_DIR}/ex02_v01_bimsim_config.json
-EX02_V01_OUTPUT=${EX02_V01_DIR}/ex02_v01_bimsim_grid_log.csv
-EX02_V02_DIR=${EX02_DIR}/v02
-EX02_V02_CONFIG=${EX02_V02_DIR}/ex02_v02_bimsim_config.json
-EX02_V02_OUTPUT=${EX02_V02_DIR}/ex02_v02_bimsim_grid_log.csv
+# Configuration 01
+EX02_C01_DIR=${EX02_DIR}/c01
+# Variation 01
+EX02_C01_V01_DIR=${EX02_C01_DIR}/v01
+EX02_C01_V01_CONFIG=${EX02_C01_V01_DIR}/ex02_c01_v01_bimsim_config.json
+EX02_C01_V01_OUTPUT=${EX02_C01_V01_DIR}/ex02_c01_v01_bimsim_grid_log.csv
+# Variation 02
+EX02_C01_V02_DIR=${EX02_C01_DIR}/v02
+EX02_C01_V02_CONFIG=${EX02_C01_V02_DIR}/ex02_c01_v02_bimsim_config.json
+EX02_C01_V02_OUTPUT=${EX02_C01_V02_DIR}/ex02_c01_v02_bimsim_grid_log.csv
+# Configuration 02
+EX02_C02_DIR=${EX02_DIR}/c02
+# Variation 01
+EX02_C02_V01_DIR=${EX02_C02_DIR}/v01
+EX02_C02_V01_CONFIG=${EX02_C02_V01_DIR}/ex02_c02_v01_bimsim_config.json
+EX02_C02_V01_OUTPUT=${EX02_C02_V01_DIR}/ex02_c02_v01_bimsim_grid_log.csv
+# Variation 02
+EX02_C02_V02_DIR=${EX02_C02_DIR}/v02
+EX02_C02_V02_CONFIG=${EX02_C02_V02_DIR}/ex02_c02_v02_bimsim_config.json
+EX02_C02_V02_OUTPUT=${EX02_C02_V02_DIR}/ex02_c02_v02_bimsim_grid_log.csv
 
-# Ex03
+# Example 03
 EX03_DIR=${EXAMPLES_DIR}/ex03
-EX03_V01_DIR=${EX03_DIR}/v01
-EX03_V01_CONFIG=${EX03_V01_DIR}/ex03_v01_bimsim_config.json
-EX03_V01_OUTPUT=${EX03_V01_DIR}/ex03_v01_bimsim_grid_log.csv
-EX03_V02_DIR=${EX03_DIR}/v02
-EX03_V02_CONFIG=${EX03_V02_DIR}/ex03_v02_bimsim_config.json
-EX03_V02_OUTPUT=${EX03_V02_DIR}/ex03_v02_bimsim_grid_log.csv
+# Configuration 01
+EX03_C01_DIR=${EX03_DIR}/c01
+# Variation 01
+EX03_C01_V01_DIR=${EX03_C01_DIR}/v01
+EX03_C01_V01_CONFIG=${EX03_C01_V01_DIR}/ex03_c01_v01_bimsim_config.json
+EX03_C01_V01_OUTPUT=${EX03_C01_V01_DIR}/ex03_c01_v01_bimsim_grid_log.csv
+# Variation 02
+EX03_C01_V02_DIR=${EX03_C01_DIR}/v02
+EX03_C01_V02_CONFIG=${EX03_C01_V02_DIR}/ex03_c01_v02_bimsim_config.json
+EX03_C01_V02_OUTPUT=${EX03_C01_V02_DIR}/ex03_c01_v02_bimsim_grid_log.csv
+# Configuration 02
+EX03_C02_DIR=${EX03_DIR}/c02
+# Variation 01
+EX03_C02_V01_DIR=${EX03_C02_DIR}/v01
+EX03_C02_V01_CONFIG=${EX03_C02_V01_DIR}/ex03_c02_v01_bimsim_config.json
+EX03_C02_V01_OUTPUT=${EX03_C02_V01_DIR}/ex03_c02_v01_bimsim_grid_log.csv
+# Variation 02
+EX03_C02_V02_DIR=${EX03_C02_DIR}/v02
+EX03_C02_V02_CONFIG=${EX03_C02_V02_DIR}/ex03_c02_v02_bimsim_config.json
+EX03_C02_V02_OUTPUT=${EX03_C02_V02_DIR}/ex03_c02_v02_bimsim_grid_log.csv
 
 
 ###############################################################################
 # Run all simulation examples
 echo -e "\nExecuting all simulation examples...\n"
 
-# Ex01
+# Ex01: High Occupancy
 echo -e "\nExecuting simulation example 01...\n"
-${SIMULATION_BINARY} -c ${EX01_V01_CONFIG} -o ${EX01_V01_OUTPUT} -d ${DEFAULT_SIM_TIME} 
-${SIMULATION_BINARY} -c ${EX01_V02_CONFIG} -o ${EX01_V02_OUTPUT} -d ${DEFAULT_SIM_TIME}
+${SIMULATION_BINARY} -c ${EX01_C01_V01_CONFIG} -o ${EX01_C01_V01_OUTPUT} -d ${DEFAULT_SIM_TIME} 
+${SIMULATION_BINARY} -c ${EX01_C01_V02_CONFIG} -o ${EX01_C01_V02_OUTPUT} -d ${DEFAULT_SIM_TIME}
+${SIMULATION_BINARY} -c ${EX01_C02_V01_CONFIG} -o ${EX01_C02_V01_OUTPUT} -d ${DEFAULT_SIM_TIME} 
+${SIMULATION_BINARY} -c ${EX01_C02_V02_CONFIG} -o ${EX01_C02_V02_OUTPUT} -d ${DEFAULT_SIM_TIME}
 
-# Ex02
+# Ex02: Half Occupancy
 echo -e "\nExecuting simulation example 02...\n"
-${SIMULATION_BINARY} -c ${EX02_V01_CONFIG} -o ${EX02_V01_OUTPUT} -d ${DEFAULT_SIM_TIME}
-${SIMULATION_BINARY} -c ${EX02_V02_CONFIG} -o ${EX02_V02_OUTPUT} -d ${DEFAULT_SIM_TIME}
+${SIMULATION_BINARY} -c ${EX02_C01_V01_CONFIG} -o ${EX02_C01_V01_OUTPUT} -d ${DEFAULT_SIM_TIME} 
+${SIMULATION_BINARY} -c ${EX02_C01_V02_CONFIG} -o ${EX02_C01_V02_OUTPUT} -d ${DEFAULT_SIM_TIME}
+${SIMULATION_BINARY} -c ${EX02_C02_V01_CONFIG} -o ${EX02_C02_V01_OUTPUT} -d ${DEFAULT_SIM_TIME} 
+${SIMULATION_BINARY} -c ${EX02_C02_V02_CONFIG} -o ${EX02_C02_V02_OUTPUT} -d ${DEFAULT_SIM_TIME}
 
-# Ex03
+# Ex03: Low Occupancy
 echo -e "\nExecuting simulation example 03...\n"
-${SIMULATION_BINARY} -c ${EX03_V01_CONFIG} -o ${EX03_V01_OUTPUT} -d ${DEFAULT_SIM_TIME}
-${SIMULATION_BINARY} -c ${EX03_V02_CONFIG} -o ${EX03_V02_OUTPUT} -d ${DEFAULT_SIM_TIME}
+${SIMULATION_BINARY} -c ${EX03_C01_V01_CONFIG} -o ${EX03_C01_V01_OUTPUT} -d ${DEFAULT_SIM_TIME} 
+${SIMULATION_BINARY} -c ${EX03_C01_V02_CONFIG} -o ${EX03_C01_V02_OUTPUT} -d ${DEFAULT_SIM_TIME}
+${SIMULATION_BINARY} -c ${EX03_C02_V01_CONFIG} -o ${EX03_C02_V01_OUTPUT} -d ${DEFAULT_SIM_TIME} 
+${SIMULATION_BINARY} -c ${EX03_C02_V02_CONFIG} -o ${EX03_C02_V02_OUTPUT} -d ${DEFAULT_SIM_TIME}
+
 
 echo -e "\n--- YOU EXECUTED ---"
